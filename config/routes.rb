84980e6e00e5b312a7ea48f42b7c2a9e7774b79b
Brenda Reyes <129559415+brendareyes-techprep@users.noreply.github.com>
2023-10-16 20:49:03 +0000

    did the path url

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,19 @@
 Rails.application.routes.draw do
-  # get "/", controller: "movies", action: "index"
-  # get "/" => "movies#index"
-  root movies#index
+  get "/" => "movies#index"
 
   # Routes for the Movie resource:
 
   # CREATE
-  post "/movies" => "movies#create" 
-  get "/movies/new" => "movies#new"
+  post "/movies" => "movies#create", as: :movies
+  get "/movies/new" => "movies#new", as: :new_movie
           
   # READ
   get "/movies" => "movies#index"
-  get "/movies/:id" => "movies#show"
+  get "/movies/:id" => "movies#show", as: :movie
   
   # UPDATE
   patch "/movies/:id" => "movies#update"
-  get "/movies/:id/edit" => "movies#edit"
+  get "/movies/:id/edit" => "movies#edit", as: :edit_movie
   
   # DELETE
   delete "/movies/:id" => "movies#destroy"
